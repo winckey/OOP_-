a80ba04973790ab3040a995414a17361ed827e05
JangHyunJin <winckey3@gmail.com>
2022-07-29 17:55:32 +0900

    영화할인 1-1

---
+++ CHAPTER2/chater2_code/src/main/java/com/oop/chater2_code/Movie_App/step01/Movie.java
@@ -0,0 +1,31 @@
+package com.oop.chater2_code.Movie_App.step01;
+
+
+
+import com.oop.chater2_code.Movie_App.money.Money;
+import com.oop.chater2_code.Movie_App.step01.pricing.DiscountPolicy;
+
+import java.time.Duration;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(String title, Duration runningTime, Money fee, DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
+
---
+++ CHAPTER2/chater2_code/src/test/java/com/oop/chater1_code/Chater1CodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oop.chater1_code;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Chater1CodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
