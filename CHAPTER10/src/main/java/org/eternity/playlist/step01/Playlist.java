e3a484f5d67efd3f6bff808e825df2e921d44a19
JangHyunJin <winckey3@gmail.com>
2022-08-20 10:37:24 +0000

    CHAPTER11

--- CHAPTER10/src/main/java/org/eternity/playlist/step01/Playlist.java
+++ CHAPTER10/src/main/java/org/eternity/playlist/step01/Playlist.java
@@ -1,10 +1,11 @@
 package org.eternity.playlist.step01;
 
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class Playlist {
-    private List<Song> tracks = new ArrayList<>();
+    private List<Song> tracks = new ArrayList();
 
     public void append(Song song) {
         getTracks().add(song);
